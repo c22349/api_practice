4cd5598ec69b3017d0a78fd986f258e3425dd44e
c22349 <takes1840@gmail.com>
2023-02-09 18:00:33 +0900

    最初のコミット

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+let booklog = {}
+
+// jsを使えるようにする
+app.use(express.json())
+
+// booklogにPOSTされた時に
+app.post('/booklog', (req, res) => {
+  booklog = req.body
+
+  if (!(booklog.name && booklog.text)) {
+    return res.json({
+      "ok": false,
+      "error": "invalid parameter"
+    })
+  }
+
+  res.json({
+    "ok": true,
+    "booklog":booklog
+  })
+})
+
+app.get("/booklog", (req, res) =>  {
+  res.json({
+    "ok": true,
+    "booklog": [
+      booklog
+    ]
+  })
+})
+
+app.listen(port, () =>{
+  console.log(`App listening at http://localhost:${port}`)
+})
